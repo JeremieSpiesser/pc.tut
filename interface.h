9ff33d811d57085724773437ee0901c1a751818c
PM <pierre-marie.lefrancois@ensiie.fr>
2020-03-11 10:21:45 +0100

    interface.h sans commentaires

--- interface.h
+++ interface.h
@@ -5,4 +5,16 @@
 #ifndef PCTUT_INTERFACE_H
 #define PCTUT_INTERFACE_H
 
+void dispNewTurn(int turnNumber);
+
+void dispNewPhase(int turnNumber, int phaseNumber, int startedWith);
+
+void dispBoard(plateau board);
+
+carte askEleve();
+
+carte askAction(plateau board);
+
+void dispWinner(plateau board);
+
 #endif //PCTUT_INTERFACE_H
