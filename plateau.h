01007f7416e2f15d81950ff32c2391eb5615b23d
Signolle <bapt.signolle@gmail.com>
2020-03-18 21:57:50 +0000

    Modification de l'interface plateau.h
    - Ajout de la fonction addCarte qui permet d'ajouter une carte au plateau (si les conditions sont reunies)
    
    - Ajout de la fonction addEleve qui permet d'ajouter une carte eleve au plateau, attention le type de l'argument est carte et non carte_eleve

--- plateau.h
+++ plateau.h
@@ -4,6 +4,7 @@
 
 #ifndef PCTUT_PLATEAU_H
 #define PCTUT_PLATEAU_H
+#include "carte.h"
 
 typedef struct plateau* plateau;
 
@@ -30,6 +31,42 @@ void nouveauTour(plateau p, int* pnt);
 //@requires un plateau correct non vide, pnt : pointe vers le numéro de tour (commence à 1)
 //@assigns rien
 //@ensures retourne nombre de cartes piochées par une ENSIIE au début de sa phase
-int nbCartesNewPhase(plateau p, int* pnt)
+int nbCartesNewPhase(plateau p, int* pnt);
+
+//@requires un plateau p valide, une carte c et un entier nt numéro de tour (commence à 1)
+//@assigns ajoute la carte a la liste des cartes en jeu
+//@ensures l'ajout de la carte sur le plateau, renvoie 0 en cas de succès, 1 sinon (si il y a déjà trop de carte personnel sur le terrain par example)
+int addCarte(plateau p, carte c, int nt);
+
+//@requires un plateau p initialise et une carte c de type 1 (cad de type eleve)
+//@assigns ajoute la carte eleve a la liste des cartes en jeu par le biais de la fonction addCarte
+//@ensures l'ajout de la carte eleve au plateau
+//La carte eleve est passee par le type carte, suivant l'usage il vaudrait peut-etre mieux la passer par le type carte_eleve
+void addEleve(plateau p, carte c);
+
+//@requires un plateau p initialise et une carte c de la main (de type action ou personnel)
+//@assigns retire c de la main pour la placer sur le terrain
+//@ensures la carte c est alors jouee
+void playCarte(plateau p, carte c);
+
+//@requires un plateau p initialise
+//@assigns rien
+//@ensures retourne le nombre de points d'energie a la disposition de l'ENSIIE du plateau
+int nbPE(plateau p);
+
+//@requires deux plateaux p1 et p2 initialises
+//@assigns rien
+//@ensures retourne 0 si la partie n'est pas terminee, 1 si p1 l'emporte, 2 si c'est p2, 3 en cas d'egalite
+int finPartie(plateau p1, plateau p2);
+
+//@requires un plateau p initialise
+//@assigns rien
+//@ensures retourne le nombre de cartes eleve qui seront ajoutees au plateau
+int nbNewElevesNewPhase(plateau p);
+
+//@requires deux plateaux p1 et p2 initialises
+//@assigns rien
+//@ensures la cloture du tour, comprend le calcul des DD de chaque plateau 
+void finTour(plateau p1, plateau p2);
 
 #endif //PCTUT_PLATEAU_H
